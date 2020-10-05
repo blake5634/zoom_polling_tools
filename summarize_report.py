8ef88b735c4de1f16a7d48e68518aaf66921d49f
Blake Hannaford <blake@uw.edu>
2020-10-04 20:18:33 -0700

    generate results plot similar to the bar graph that pops up in the meeting

--- summarize_report.py
+++ summarize_report.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 import csv
 import sys
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    csv_file = parse_args()
+    args = parse_args()
+    csv_file = args[0]
+    mtg_name = args[1]
     questions = dict()
-
     with open(csv_file) as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             num_cells = len(row)
 
             # first question is in column 4. zoom always adds an empty cell/additional comma.
-            if num_cells <= 5:
+            if num_cells <= 5:  # this catches the header comment row
                 continue
-
+            datetimestr = row[3]
             for i in range(4, num_cells - 1, 2):
                 question = row[i]
                 answer = row[i + 1]
@@ -24,18 +26,43 @@ def main():
                     questions[question] = Question(question)
                 questions[question].record_answer(answer)
 
+    poll = Poll(questions,mtg_name,datetimestr)
     for question in questions.values():
         question.print()
+    poll.plot()
+         
+    plt.show()
+
+    
 
 
 def parse_args():
+    meetingName = 'In Meeting'
     if len(sys.argv) < 2:
-        sys.stderr.write("Usage: %s <zoom poll report csv file>\n" % (sys.argv[0]))
+        sys.stderr.write("Usage: %s <zoom poll report csv file> [meeting title]\n" % (sys.argv[0]))
         sys.stderr.flush()
         sys.exit(1)
-    return sys.argv[1]
+    if len(sys.argv) == 3:
+        meetingName = sys.argv[2]
+    return [ sys.argv[1], meetingName]
 
-
+class Poll:
+    def __init__(self,Qs,mn,ds):
+        self.questions = Qs  # dict{str:Question}
+        self.nq = len(Qs)
+        self.date_time_str = ds
+        self.mtg_name = mn
+        
+    def plot(self):
+        plotsize = 3
+        vsize = plotsize*len(self.questions)
+        fig, axs = plt.subplots(self.nq,1,constrained_layout=True,sharex=True)
+        fig.suptitle( self.mtg_name + ' Zoom poll '+self.date_time_str, fontsize=16)
+        j=0
+        for q in list( self.questions.values()):
+            q.graph(fig,axs[j])
+            j+=1
+            
 class Question:
     def __init__(self, question):
         self.question = question
@@ -50,8 +77,26 @@ class Question:
         print(self.question)
         for answer in sorted(list(self.answers.keys())):
             print("%s: %s" % (answer, self.answers[answer]))
-        print()
-
+        print() 
+            
+        
+    def graph(self,fig,ax): 
+        ansl    = list(self.answers.keys())
+        counts = list(self.answers.values())
+        ac = zip(counts,ansl)
+        ans_sort = [x for _,x in sorted(ac,reverse=True)]
+        c_sort = sorted(counts,reverse=True)
+        ansl = ans_sort
+        counts = c_sort
+        y_pos = np.arange(len(ansl)) 
+        ax.barh(y_pos, counts, align='center')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(ansl)
+        ax.invert_yaxis()  # labels read top-to-bottom
+        ax.set_xlabel('Number of Responses')
+        ax.set_title(self.question)
+        
+       
 
 if __name__ == '__main__':
     main()
